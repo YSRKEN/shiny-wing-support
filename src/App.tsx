54282b3ad262bc80ad25b0cb53d112f570f71e0c
YSRKEN <ysr.ken@gmail.com>
2020-04-25 18:51:09 +0900

    [add] タブを生やした

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Tabs, Tab } from 'react-bootstrap';
 import 'App.css';
 
 const App: React.FC = () => (
   <Container>
     <Row className="my-3">
       <Col>
-        <h1 className="text-center">シャニマス WING 計算機</h1>
+        <h1 className="text-center d-none d-sm-block">シャニマス WING 計算機</h1>
+        <h1 className="text-center d-block d-sm-none">WING 計算機</h1>
+      </Col>
+    </Row>
+    <Row className="my-3">
+      <Col>
+        <Tabs defaultActiveKey="unit" id="application-mode" transition={false}>
+          <Tab eventKey="unit" title="ユニット編成">
+            <span>aaa</span>
+          </Tab>
+          <Tab eventKey="wing" title="WINGシミュレーション">
+            <span>aaa</span>
+          </Tab>
+        </Tabs>
       </Col>
     </Row>
   </Container>
