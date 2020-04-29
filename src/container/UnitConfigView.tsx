9f63eb217522532a4c42fa9e364de5227a686c7e
YSRKEN <ysr.ken@gmail.com>
2020-04-29 12:47:24 +0900

    [clean] ソースコードにコメントを追加。また、わかりにくかったボタンの無効化条件を別途分離

--- src/container/UnitConfigView.tsx
+++ src/container/UnitConfigView.tsx
@@ -5,11 +5,14 @@ import { UnitConfigContext } from 'state/UnitConfigState';
 
 // ユニット編成入力用フォーム
 const UnitConfigView: React.FC = () => {
+  // ユニット一覧、入力しているユニットの情報、dispatch
   const { supportUnitList, selectedSupportUnit, dispatch } = useContext(UnitConfigContext);
-
+  // 現在選択しているユニット名
   const [unitName, setUnitName] = useState('');
 
+  // unitNameの自動更新
   useEffect(() => {
+    // ユニット一覧が更新され、その中にunitNameが該当しない場合、unitNameを自動で修正する
     if (supportUnitList.length > 0) {
       if (unitName === '' || !supportUnitList.map(r => r.unitName).includes(unitName)) {
         setUnitName(supportUnitList[0].unitName);
@@ -18,28 +21,19 @@ const UnitConfigView: React.FC = () => {
   // eslint-disable-next-line
   }, [supportUnitList]);
 
+  // unitNameを変更
   const selectSupportUnit = (e: FormEvent<any>) => {
     setUnitName(e.currentTarget.value);
   }
-  const onChangeUnitName = (e: FormEvent<any>) => {
-    dispatch({type: 'setUnitName', message: e.currentTarget.value});
-  };
 
-  const setIdolName = (index: number, name: string) => {
-    dispatch({type: 'setIdolName', message: `${index},${name}`});
-  };
+  // 入力しているユニットの情報が変更された際の処理
+  const onChangeUnitName = (e: FormEvent<any>) => dispatch({type: 'setUnitName', message: e.currentTarget.value});
+  const setIdolName = (index: number, name: string) => dispatch({type: 'setIdolName', message: `${index},${name}`});
+  const setIdolVocal = (index: number, vocal: number) => dispatch({type: 'setIdolVocal', message: `${index},${vocal}`});
+  const setIdolDance = (index: number, dance: number) => dispatch({type: 'setIdolDance', message: `${index},${dance}`});
+  const setIdolVisual= (index: number, visual: number) => dispatch({type: 'setIdolVisual', message: `${index},${visual}`});
 
-  const setIdolVocal = (index: number, vocal: number) => {
-    dispatch({type: 'setIdolVocal', message: `${index},${vocal}`});
-  };
-
-  const setIdolDance = (index: number, dance: number) => {
-    dispatch({type: 'setIdolDance', message: `${index},${dance}`});
-  };
-
-  const setIdolVisual= (index: number, visual: number) => {
-    dispatch({type: 'setIdolVisual', message: `${index},${visual}`});
-  };
+  // 保存済みユニットを追加・読込み・上書き・削除する処理
   const addUnit = () => dispatch({ type: 'addUnit', message: '' });
   const loadUnit = () => dispatch({ type: 'loadUnit', message: unitName });
   const updateUnit = () => dispatch({ type: 'updateUnit', message: unitName });
@@ -48,6 +42,23 @@ const UnitConfigView: React.FC = () => {
       dispatch({ type: 'deleteUnit', message: unitName });
     }
   };
+
+  // 各ボタンの無効化フラグ
+  // flg1：保存済みユニットの名前と、入力しているユニットの名前が被っている
+  const flg1 = supportUnitList.map(r => r.unitName).includes(selectedSupportUnit.unitName);
+  // flg2：入力しているユニットの名前が空
+  const flg2 = selectedSupportUnit.unitName === ''
+  // flg3：選択している保存済みユニットの名前が空
+  const flg3 = unitName === '';
+
+  // 追加ボタン：flg1 or flg2
+  const disabledAdd = flg1 || flg2;
+  // 読込みボタン：flg3
+  const disabledLoad = flg3;
+  // 追加ボタン：(flg1 and 選択している保存済みユニットの名前≠入力しているユニットの名前) or flg2
+  const disabledUpdate = (flg1 && selectedSupportUnit.unitName !== unitName) || flg2;
+  // 削除ボタン：flg3 or 保存済みユニットが空
+  const disabledDelete = flg3 || supportUnitList.length === 0;
 
   return (
     <Form>
@@ -96,14 +107,10 @@ const UnitConfigView: React.FC = () => {
           {supportUnitList.map((unit) => <option key={unit.unitName} value={unit.unitName}>{unit.unitName}</option>)}
         </Form.Control>
         <div className="my-3">
-          <Button className="mr-3" onClick={addUnit}
-            disabled={supportUnitList.map(r => r.unitName).includes(selectedSupportUnit.unitName) || selectedSupportUnit.unitName === ''}>追加</Button>
-          <Button className="mr-3" onClick={loadUnit} variant="warning"
-            disabled={unitName === ''}>読込み</Button>
-          <Button className="mr-3" onClick={updateUnit} variant="warning"
-            disabled={(supportUnitList.map(r => r.unitName).includes(selectedSupportUnit.unitName) && selectedSupportUnit.unitName !== unitName) || selectedSupportUnit.unitName === ''}>上書き</Button>
-          <Button  onClick={deleteUnit} variant="danger"
-             disabled={unitName === '' || supportUnitList.length === 0}>削除</Button>
+          <Button onClick={addUnit} disabled={disabledAdd} className="mr-3">追加</Button>
+          <Button onClick={loadUnit} disabled={disabledLoad} className="mr-3" variant="warning">読込み</Button>
+          <Button onClick={updateUnit} disabled={disabledUpdate} className="mr-3" variant="warning">上書き</Button>
+          <Button onClick={deleteUnit} disabled={disabledDelete} variant="danger">削除</Button>
         </div>
       </Form.Group>
     </Form>
